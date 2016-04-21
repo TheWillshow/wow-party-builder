f1dfe19c24bf9b6dce9d2c1497d325eb218dfe01
Will <will@resilientcoders.org>
2016-04-21 13:34:47 -0400

    added more tests

--- spec/features/user_can_find_guild_guildmembers_spec.rb
+++ spec/features/user_can_find_guild_guildmembers_spec.rb
@@ -25,4 +25,34 @@ feature 'User can find guild and guildmembers' do
     expect(page).to have_content('Guildname')
     expect(page).to have_content('Realm')
   end
+
+  scenario 'logged in user can input guild and server to find guild' do
+    click_on 'Sign In'
+    fill_in 'Email', with: 'toothlessthedragon@email.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+
+    fill_in 'guild_guild_name', with: 'Last Pull'
+    select 'Illidan', from: 'guild[realm]'
+    click_on 'Submit'
+
+    expect(page).to have_content('Guild has been imported')
+  end
+
+  scenario 'logged in user can not import a guild twice' do
+    click_on 'Sign In'
+    fill_in 'Email', with: 'toothlessthedragon@email.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+
+    fill_in 'guild_guild_name', with: 'Last Pull'
+    select 'Illidan', from: 'guild[realm]'
+    click_on 'Submit'
+
+    fill_in 'guild_guild_name', with: 'Last Pull'
+    select 'Illidan', from: 'guild[realm]'
+    click_on 'Submit'
+
+    expect(page).to have_content('Guild already in Database')
+  end
 end
